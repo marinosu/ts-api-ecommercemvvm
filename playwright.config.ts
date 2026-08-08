fbdb7f232b9a60e9723b28b344ea1c7dc523d664
marinosu <msantos0138@gmail.com>
2026-08-08 18:41:09 -0500

    test E2E + Playwright + BDD v1

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,9 +12,9 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './tests/e2e/',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -22,32 +22,46 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ["list"],
+    ["html", { outputFolder: "reports/playwright" }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    /**
+     * Verificar las variables HOST y PORT
+     * en las variables de entorno .env
+     */
+    baseURL: 'http://192.168.1.8:3050',
+    headless: true,
+
+    screenshot: "only-on-failure",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
   projects: [
-    {
+    /**
+     * No es necesario esta configuración
+     * ya que no se esta haciendo E2E de interfaz, sino un E2E/API test
+     */
+    /*{
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
+    },*/
 
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
+    /*{
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
